e0c68da6b1bfd59965f5fe17b1ae16852bbca906
Rikayla <Rikaylaj@gmail.com>
2022-09-03 23:36:31 -0400

    added tag and category routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,12 +7,52 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include:{
+      model:Category,
+      attributes: ['category_name']
+    },
+      include:{
+        model: Tag,
+        attributes: ['tag_name']
+      }
+  })
+  .then(gotProducts => res.json(gotProducts))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Category,
+      attributes:['category_name']
+    },
+    include:{
+      model:Tag,
+      attributes:['tag_name']
+
+    }
+  })
+  .then(gotProduct => {
+    if(!gotProduct) {
+      res.status(404).json({message: 'No product found with this id'});
+      return;
+    }
+    res.json(gotProduct);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
